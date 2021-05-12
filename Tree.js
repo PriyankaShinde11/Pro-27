a102348baebbf6a671cea3430b9fbed64d71187f
PriyankaShinde11 <76612119+PriyankaShinde11@users.noreply.github.com>
2021-05-12 12:52:51 +0530

    Add files via upload

---
+++ Tree.js
@@ -0,0 +1,10 @@
+class Baseclass{
+    constructor(x,y,width,height){
+        var options = {
+            'isStatic' : true,
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+    }
+}
